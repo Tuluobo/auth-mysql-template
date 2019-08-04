be957c2182e3653aea9c13a70c41ba0657ce3f46
Tuluobo <tuluobo@vip.qq.com>
2019-08-04 21:16:15 +0800

    [MOD] replace SQLite to MySQL

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,11 @@
 import Authentication
-import FluentSQLite
+import FluentMySQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentMySQLProvider())
     try services.register(AuthenticationProvider())
 
     // Register routes to the router
@@ -20,20 +20,21 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    // Configure a MySQL database
+    let mysqlConfig = MySQLDatabaseConfig(hostname: "localhost", port: 3306, username: "root", password: "123456", database: "auth", capabilities: .default, characterSet: .utf8_general_ci, transport: .cleartext)
+    let mysql = MySQLDatabase(config: mysqlConfig)
 
-    // Register the configured SQLite database to the database config.
+    // Register the configured MySQL database to the database config.
     var databases = DatabasesConfig()
-    databases.enableLogging(on: .sqlite)
-    databases.add(database: sqlite, as: .sqlite)
+    databases.enableLogging(on: .mysql)
+    databases.add(database: mysql, as: .mysql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: User.self, database: .sqlite)
-    migrations.add(model: UserToken.self, database: .sqlite)
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: User.self, database: .mysql)
+    migrations.add(model: UserToken.self, database: .mysql)
+    migrations.add(model: Todo.self, database: .mysql)
     services.register(migrations)
 
 }
